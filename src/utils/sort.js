6a12c92a8e1ceca57f42b146fae19377507dba6d
aniler0 <anil.er@hotmail.com>
2021-12-28 23:41:54 +0200

    sort method changed

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -1,7 +1,9 @@
 export const sort = (data) => {
-  return data.sort((a, b) => {
+  const sorted = data.sort((a, b) => {
     var textA = a.name.toUpperCase();
     var textB = b.name.toUpperCase();
     return textA < textB ? -1 : textA > textB ? 1 : 0;
   });
+
+  return sorted;
 };
